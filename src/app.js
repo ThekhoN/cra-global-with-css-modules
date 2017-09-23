856d067c08580940954a877691fc9b4f820ecde6
ThekhoN <bennetonss@hotmail.com>
2017-09-23 10:13:48 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './styles-global/style.global.css';
+import Test from './components/test';
+import './styles-global/style.global.css';
+
+const App = () => (
+	<div>
+		<Test />
+	</div>
+);
+
+export default App;
---
+++ src/components/test/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.css';
+
+const Test = () => (
+	<div className={`${styles.test} global-special`}>
+		<h2>Test</h2>
+	</div>
+);
+
+export default Test;
